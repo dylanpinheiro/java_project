39c876f4e89e482d9f6f7c41b28038a31db908e2
dylanpinheiro <dylanpinheiro@icloud.com>
2015-12-13 18:30:34 +0530

    Create CallByReference.java
    4
    5

---
+++ CallByReference.java
@@ -0,0 +1,18 @@
+class Number 
+{
+   int x;
+}
+
+public class CallByReference 
+{
+
+   public static void main (String args[]) 
+   {
+      Number a = new Number();
+      a.x=4;
+      System.out.println(a.x);
+      Number b=a;
+      b.x=5;
+      System.out.println(b.x);
+   }
+} 
